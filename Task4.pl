4652f778223dfb0da3538716e329333237187c00
Artyom <super.a-piliugin@yandex.ru>
2025-10-07 23:16:53 +0300

    task4 update

--- Task4.pl
+++ Task4.pl
@@ -4,59 +4,67 @@ use warnings;
 
 # Задание 4: Лексикографически упорядоченный список лексем на основе хеш-массива
 
-my %my_hash = (
-    "ФИО" => "Иванов И.И.",
-    "Курс" => "4",
-    "Группа" => "96вп3"
-);  # Инициализация хеша примером из теоретической части
-my @sorted_keys = sort keys %my_hash;  # Упорядоченный список ключей
+my %hash = ("enter" => "");  # Initialize linked list with sentinel node
 
 # Функция добавления лексемы
 sub add_lexeme {
-    my ($key, $value) = @_;
-    
-    # Проверка, существует ли ключ
-    if (exists $my_hash{$key}) {
-        print "Лексема '$key' уже существует. Обновить значение? (y/n): ";
-        chomp(my $choice = <STDIN>);
-        return if $choice ne 'y';
+    my ($new_string) = @_;
+    my $prev = "enter";
+    my $current = $hash{$prev};
+
+    # Traverse to find insertion point
+    while ($current && $current lt $new_string) {
+        $prev = $current;
+        $current = $hash{$current};
     }
-    
-    # Добавление или обновление пары ключ-значение
-    $my_hash{$key} = $value;
-    
-    # Обновление упорядоченного списка ключей
-    @sorted_keys = sort keys %my_hash;
-    print "Лексема '$key' добавлена/обновлена.\n";
+
+    # Check for duplicate
+    if ($current && $current eq $new_string) {
+        print "Лексема '$new_string' уже существует.\n";
+        return;
+    }
+
+    # Insert new lexeme
+    $hash{$prev} = $new_string;
+    $hash{$new_string} = $current;
+    print "Лексема '$new_string' добавлена.\n";
 }
 
 # Функция удаления лексемы
 sub delete_lexeme {
-    my ($key) = @_;
-    
-    if (!exists $my_hash{$key}) {
-        print "Лексема '$key' не найдена.\n";
-        return;
+    my ($del_string) = @_;
+    my $prev = "enter";
+    my $current = $hash{$prev};
+
+    # Traverse to find lexeme to delete
+    while ($current && $current ne $del_string) {
+        $prev = $current;
+        $current = $hash{$current};
     }
-    
-    # Удаление лексемы
-    delete $my_hash{$key};
-    
-    # Обновление упорядоченного списка ключей
-    @sorted_keys = sort keys %my_hash;
-    print "Лексема '$key' удалена.\n";
+
+    if ($current) {
+        $hash{$prev} = $hash{$current};
+        delete $hash{$current};
+        print "Лексема '$del_string' удалена.\n";
+    } else {
+        print "Лексема '$del_string' не найдена.\n";
+    }
 }
 
 # Функция просмотра списка
 sub view_list {
-    if (!%my_hash) {
+    my $current = $hash{"enter"};
+    if (!$current) {
         print "Список пуст.\n";
         return;
     }
-    
+
     print "Список лексем:\n";
-    foreach my $key (@sorted_keys) {
-        print "$key => $my_hash{$key}\n";
+    my %visited;  # Track visited nodes to prevent loops
+    while ($current && !$visited{$current}) {
+        print "$current\n";
+        $visited{$current} = 1;
+        $current = $hash{$current};
     }
 }
 
@@ -66,26 +74,24 @@ while (1) {
     print "1. Добавить лексему\n";
     print "2. Удалить лексему\n";
     print "3. Просмотреть список\n";
-    print "0. Выход\n";
-    print "Выберите опцию: ";
+    print "4. Выход\n";
+    print "Выберите действие: ";
     chomp(my $choice = <STDIN>);
-    
-    last if $choice eq '0';
-    
-    if ($choice eq '1') {
-        print "Введите ключ лексемы: ";
-        chomp(my $key = <STDIN>);
-        print "Введите значение лексемы: ";
-        chomp(my $value = <STDIN>);
-        add_lexeme($key, $value);
-    } elsif ($choice eq '2') {
-        print "Введите ключ лексемы для удаления: ";
-        chomp(my $key = <STDIN>);
-        delete_lexeme($key);
-    } elsif ($choice eq '3') {
-        print "Список лексем:\n";
+
+    if ($choice == 1) {
+        print "Введите лексему для добавления: ";
+        chomp(my $new_string = <STDIN>);
+        add_lexeme($new_string) if $new_string;  # Only add non-empty strings
+    } elsif ($choice == 2) {
+        print "Введите лексему для удаления: ";
+        chomp(my $del_string = <STDIN>);
+        delete_lexeme($del_string) if $del_string;  # Only attempt to delete non-empty strings
+    } elsif ($choice == 3) {
         view_list();
+    } elsif ($choice == 4) {
+        print "Выход.\n";
+        last;
     } else {
-        print "Неверный выбор.\n";
+        print "Неверный выбор. Попробуйте снова.\n";
     }
 }
